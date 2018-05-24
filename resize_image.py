e3d1cd83f52dbe040ada9f163defb4c6183675a8
blitzingeagle <mcmorrischen@gmail.com>
2018-05-24 10:47:31 +0800

    Inter Cubic Resize

--- resize_image.py
+++ resize_image.py
@@ -3,7 +3,11 @@ import cv2
 
 if __name__ == "__main__":
     img = cv2.imread("test_input.png")[:,:,::-1]
-    cv2.imshow("Image", img)
+    cv2.imshow("img", img)
+
+    res = cv2.resize(img, dsize=(128,128), interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("resized", res)
+
     cv2.waitKey()
     cv2.destroyAllWindows()
 
