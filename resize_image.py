e9f9d4c44707fdf18e10e6c2140cb400e3241b16
blitzingeagle <mcmorrischen@gmail.com>
2018-05-24 10:13:45 +0800

    Opencv BGR to RGB

--- resize_image.py
+++ resize_image.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 if __name__ == "__main__":
-    img = cv2.imread("test_input.png")
+    img = cv2.imread("test_input.png")[:,:,::-1]
     cv2.imshow("Image", img)
     cv2.waitKey()
     cv2.destroyAllWindows()
