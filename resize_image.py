f85c48521b2994f67ab447525607180259f4c2cc
blitzingeagle <mcmorrischen@gmail.com>
2018-05-24 12:04:50 +0800

    Resize to mean

--- resize_image.py
+++ resize_image.py
@@ -13,16 +13,18 @@ def resize_images(images, shape=(128.128)):
     return [cv2.resize(img, dsize=shape, interpolation=cv2.INTER_CUBIC) for img in images]
 
 
+def resize_to_mean(imageset):
+    imgset_stats = imageset_stats(imageset)
+    target_shape = (int(imgset_stats["shape"]["mean_width"]), int(imgset_stats["shape"]["mean_height"]))
+    return resize_images(imageset, shape=target_shape)
+
+
 if __name__ == "__main__":
     input_dir = "images"
     output_dir = "data"
 
     imageset = load_imageset(input_dir)
-    imgset_stats = imageset_stats(imageset)
+    outputset = resize_to_mean(imageset)
 
-    target_shape = (int(imgset_stats["shape"]["mean_width"]), int(imgset_stats["shape"]["mean_height"]))
-    print("Target shape:", target_shape)
-
-    outputset = resize_images(imageset, shape=target_shape)
     for idx, img in enumerate(outputset):
         cv2.imwrite(path.join(output_dir, "%05d.png" % idx), img)
