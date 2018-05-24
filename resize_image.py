fa5c327fb3b5f5a512c0c048ed03ffdf11200198
blitzingeagle <mcmorrischen@gmail.com>
2018-05-24 11:26:03 +0800

    Make image resizer

--- resize_image.py
+++ resize_image.py
@@ -1,18 +1,28 @@
+from __future__ import print_function
+
+from load_imageset import load_imageset
+from imageset_stats import imageset_stats
+
 import numpy as np
 import cv2
 
+import os.path as path
 
-def resize_images(images, dsize=(128.128)):
-    return [cv2.resize(img, dsize=dsize, interpolation=cv2.INTER_CUBIC) for img in images]
+
+def resize_images(images, shape=(128.128)):
+    return [cv2.resize(img, dsize=shape, interpolation=cv2.INTER_CUBIC) for img in images]
 
 
 if __name__ == "__main__":
-    img = cv2.imread("test_input.png")[:,:,::-1]
-    cv2.imshow("img", img)
+    input_dir = "images"
+    output_dir = "data"
 
-    res = cv2.resize(img, dsize=(128,128), interpolation=cv2.INTER_CUBIC)
-    cv2.imshow("resized", res)
+    imageset = load_imageset(input_dir)
+    imgset_stats = imageset_stats(imageset)
 
-    cv2.waitKey()
-    cv2.destroyAllWindows()
+    target_shape = (int(imgset_stats["shape"]["mean_width"]), int(imgset_stats["shape"]["mean_height"]))
+    print("Target shape:", target_shape)
 
+    outputset = resize_images(imageset, shape=target_shape)
+    for idx, img in enumerate(outputset):
+        cv2.imwrite(path.join(output_dir, "%05d.png" % idx), img)
