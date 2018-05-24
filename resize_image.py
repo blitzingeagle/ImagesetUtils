af89da81feef8dc742fbee084b83c8ee6c32696c
blitzingeagle <mcmorrischen@gmail.com>
2018-05-24 10:51:49 +0800

    Resize Image

--- resize_image.py
+++ resize_image.py
@@ -1,6 +1,11 @@
 import numpy as np
 import cv2
 
+
+def resize_images(images, dsize=(128.128)):
+    return [cv2.resize(img, dsize=dsize, interpolation=cv2.INTER_CUBIC) for img in images]
+
+
 if __name__ == "__main__":
     img = cv2.imread("test_input.png")[:,:,::-1]
     cv2.imshow("img", img)
